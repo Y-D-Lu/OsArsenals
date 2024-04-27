925bd0c558690bb0f573814e7f1beb92f3ae4bb3
Y-D-Lu <460999218@qq.com>
2024-04-28 00:12:06 +0800

    add overview view layout and fix xml codestyle

--- app/src/main/java/cn/arsenals/osarsenals/views/OverviewView.java
+++ app/src/main/java/cn/arsenals/osarsenals/views/OverviewView.java
@@ -13,7 +13,6 @@ public class OverviewView extends RelativeLayout {
     private static final String TAG = "OverviewView";
 
     private View view;
-    private View monitorView;
 
     public OverviewView(Context context) {
         super(context);
@@ -39,6 +38,5 @@ public class OverviewView extends RelativeLayout {
         Alog.debug(TAG, "initView");
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         view = inflater.inflate(R.layout.overview_view, this);
-        monitorView = findViewById(R.id.overview_monitorview);
     }
 }
