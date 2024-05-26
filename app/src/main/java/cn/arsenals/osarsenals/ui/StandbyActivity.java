05fdc2d19e4c77c8f95c60f4e459eb5383faf745
Y-D-Lu <460999218@qq.com>
2024-05-26 15:15:28 +0800

    show time in standby activity

--- app/src/main/java/cn/arsenals/osarsenals/ui/StandbyActivity.java
+++ app/src/main/java/cn/arsenals/osarsenals/ui/StandbyActivity.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.time.LocalTime;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import cn.arsenals.osarsenals.R;
 
 public class StandbyActivity extends AppCompatActivity {
+    private static final String TAG = "StandbyActivity";
+
+    private TextView standbyTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,25 @@ public class StandbyActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+
+        standbyTextview = findViewById(R.id.standby_textview);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                onTimerScheduled();
+            }
+        }, 0, 1 * 60 * 1000);
+    }
+
+    private void onTimerScheduled() {
+        LocalTime localTime = LocalTime.now();
+        standbyTextview.post(new Runnable() {
+            @Override
+            public void run() {
+                standbyTextview.setText(localTime.toString().substring(0, 5));
+            }
+        });
     }
 }
