af8c698a9890dff818643c1efb2f0aecd5ce0c10
Y-D-Lu <460999218@qq.com>
2024-08-02 00:42:57 +0800

    update standby per mintue and only show from 08:00 to 08:59

--- app/src/main/java/cn/arsenals/osarsenals/ui/StandbyActivity.java
+++ app/src/main/java/cn/arsenals/osarsenals/ui/StandbyActivity.java
@@ -2,6 +2,11 @@ package cn.arsenals.osarsenals.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -12,11 +17,14 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import cn.arsenals.osarsenals.R;
+import cn.arsenals.osarsenals.utils.Alog;
 
 public class StandbyActivity extends AppCompatActivity {
     private static final String TAG = "StandbyActivity";
 
     private TextView standbyTextview;
+
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +45,7 @@ public class StandbyActivity extends AppCompatActivity {
             public void run() {
                 onTimerScheduled();
             }
-        }, 0, 1 * 60 * 1000);
+        }, 0, 1 * 1 * 1000);
     }
 
     private void onTimerScheduled() {
@@ -45,6 +53,12 @@ public class StandbyActivity extends AppCompatActivity {
         standbyTextview.post(new Runnable() {
             @Override
             public void run() {
+                Alog.info(TAG, "onTimerScheduled " + localTime.toString());
+                if (localTime.toString().substring(0, 2).equals("08")) {
+                    standbyTextview.setVisibility(View.VISIBLE);
+                } else {
+                    standbyTextview.setVisibility(View.INVISIBLE);
+                }
                 standbyTextview.setText(localTime.toString().substring(0, 5));
             }
         });
